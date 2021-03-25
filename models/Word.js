aa4e4423c72e849e21e11eb181c67d6a6305da95
Natalia <59486546+talcia@users.noreply.github.com>
2021-03-25 20:57:37 +0100

    add word model

---
+++ models/Word.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const wordSchema = new Schema({
+	word: {
+		type: String,
+		require: true,
+	},
+	translation: {
+		type: String,
+		require: true,
+	},
+	creator: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		require: true,
+	},
+});
+
+module.exports = mongoose.model("Word", wordSchema);
