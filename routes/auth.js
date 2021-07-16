0ed8e7c096025903c29427e868ca6e468b66361a
Natalia <59486546+talcia@users.noreply.github.com>
2021-07-16 16:21:36 +0200

    fix importing

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
-const User = require('../models/user');
 
 const router = express.Router();
 
