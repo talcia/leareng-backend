6641b84413d0bfa98759972aeb558b36dd83be7c
Natalia <59486546+talcia@users.noreply.github.com>
2021-07-21 23:24:01 +0200

    fix allowed origins

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const favRoutes = require('./routes/favourites');
 
 const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8ypuv.mongodb.net/leareng?retryWrites=true&w=majority`;
 
-const allowedOrigins = [
-	'*',
-	// 'http://localhost:3000',
-	// 'https://sendgrid.api-docs.io',
-	// 'https://leareng.netlify.app',
-];
+const allowedOrigins = '*';
+// [
+// 	'http://localhost:3000',
+// 	'https://sendgrid.api-docs.io',
+// 	'https://leareng.netlify.app',
+// ];
 
 const options = {
 	origin: allowedOrigins,
